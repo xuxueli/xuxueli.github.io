ca51fbfb27298dd45b8e6b528a5377939278fe7f
xuxueli <931591021@qq.com>
2019-10-23 15:48:34 +0800

    update document

--- tool/nav/main.js
+++ tool/nav/main.js
@@ -16,6 +16,7 @@
         localStorage.searchEngine = 'https://www.baidu.com/s?wd='
         localStorage[86] = 'https://www.v2ex.com'
         localStorage[88] = 'http://www.xuxueli.com'
+        localStorage[90] = 'https://www.zhihu.com/hot'
     }
 
     for (var i in $keys) {
